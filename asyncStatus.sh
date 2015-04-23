eecbd3139b264bb606ebd15338603e25b2f33fdb
Steve Nester <steve@sahfor.com>
2015-04-23 00:17:42 -0700

    async status

---
+++ asyncStatus.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+curl -G https://test.oppwa.com/v1/payments/8a8294494ce19bdf014ce509f20b13e7 \
+  -d "authentication.userId=8a8294174b7ecb28014b9699220015cc" \
+  -d "authentication.password=sy6KJsT8" \
+  -d "authentication.entityId=8a8294174b7ecb28014b9699a3cf15d1"
