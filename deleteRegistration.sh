e0b16e0bf5a97b7b32101fed0c92caa1ab1ef0d1
Steve Nester <steve@sahfor.com>
2015-04-29 04:02:49 -0700

    delete rg

---
+++ deleteRegistration.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+curl -X DELETE "https://test.oppwa.com/v1/registrations/8a8294494ce5e0c1014ce6c11bcf453d\
+?authentication.userId=8a8294174b7ecb28014b9699220015cc\
+&authentication.password=sy6KJsT8\
+&authentication.entityId=8a8294174b7ecb28014b9699a3cf15d1"
